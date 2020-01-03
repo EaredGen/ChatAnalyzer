8021f44938405c01cd89d6eeb463544bd62c8e4f
Alexey Fedotov <alexeyfedotov1998@gmail.com>
2020-01-03 20:34:23 +0300

    First working version without patterns of programming and SOLID

---
+++ ChatAnalyzer/Chat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAnalyzer
+{
+    class Chat
+    {
+        public DateTime dateOfChat;
+        public string loginOfEmployee;
+        public string nameOfEmployee;
+        public string numberOfCustomer;
+        public string historyOfChat;
+
+        public Chat(DateTime dateOfChat, string loginOfEmployee, 
+            string nameOfEmployee, string numberOfCustomer, string historyOfChat)
+        {
+            this.dateOfChat = dateOfChat;
+            this.loginOfEmployee = loginOfEmployee;
+            this.nameOfEmployee = nameOfEmployee;
+            this.numberOfCustomer = numberOfCustomer;
+            this.historyOfChat = historyOfChat;
+        }
+
+    }
+}
---
+++ ChatAnalyzer/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAnalyzer
+{
+    class Message
+    {
+        public char owner;
+        public TimeSpan time;
+
+        public Message(char owner, TimeSpan time)
+        {
+            this.owner = owner;
+            this.time = time;
+        }
+    }
+}
